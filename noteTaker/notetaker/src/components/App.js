531455403f94b9286e74853e22f46809da961bb6
David McDeavitt <davidmcdeavitt@Davids-MacBook-Air.local>
2017-08-22 10:48:56 +1000

    capturing data

--- noteTaker/notetaker/src/components/App.js
+++ noteTaker/notetaker/src/components/App.js
@@ -2,15 +2,42 @@ import React, {Component} from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: '',
+            notes: []
+        }
+    }
+
+    submit() {
+        const notes = this.state.notes;
+
+        const newNote = { text: this.state.text };
+
+        notes.push(newNote);
+
+        this.setState({ notes: notes });
+
+    }
+
     render() {
         return (
             <div>
                 <h2>Note to Self</h2>
-                <Form>
-                    <FormControl />
-                    <Button>Submit</Button>
+                <Form inline>
+                    <FormControl onChange={event => this.setState({ text: event.target.value })} />
+                        {' '}
+                    <Button onClick={() => this.submit()}>Submit</Button>
                 </Form>
-                    
+                {
+                    this.state.notes.map((note, index) => {
+                      return(
+                          <div key={index}>{note.text}</div>
+                      )  
+                    })
+                }
             </div>
         )
     }
--- noteTaker/notetaker/src/index.js
+++ noteTaker/notetaker/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App, { color, number } from './components/App';
+import App from './components/App';
+import './index.css';
 
 // console.log(color, number);
 
